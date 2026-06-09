9d291634f4a25664c857d616b6434b1e0be3421c
maheshmahi20084-rgb <maheshmahi20084@gmail.com>
2026-06-09 07:49:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function register(){
+
+const user={
+name:document.getElementById("name").value,
+email:document.getElementById("email").value,
+password:document.getElementById("password").value
+};
+
+localStorage.setItem("user",JSON.stringify(user));
+
+alert("Registration Successful");
+
+window.location="login.html";
+}
+
+function login(){
+
+let user=
+JSON.parse(localStorage.getItem("user"));
+
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
+
+if(
+user &&
+user.email===email &&
+user.password===password
+){
+window.location="dashboard.html";
+}
+else{
+alert("Invalid Login");
+}
+}
